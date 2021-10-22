fdd907a2a38d6d15cbd134b72e293ebfdecb0d5f
ching-yi-hsu <kup1125@outlook.com>
2021-10-23 00:41:23 +0800

    building todo list

---
+++ Practise_ToDo-List/Scripts/todolist.js
@@ -0,0 +1,43 @@
+﻿
+
+var todolist = new Vue({
+    el: "#todo",
+    data: {
+        todos: [
+            { text: "Learn Code" },
+            { text: "Learn English" },
+            { text: "Watch Movie" }
+        ],
+        inputValue: ''
+    },
+    methods: {
+        addTodo: function () {
+            this.todos.push({ text: this.inputValue })
+            this.inputValue = ''
+        },
+        clearAll: function () {
+            this.todos = []
+        },
+        removeTodo: function (index) {
+            this.todos.splice(index,1)
+
+        }
+    }
+
+    
+
+})
+
+/*methods: {
+    addTodo: function () {
+        this.todos.push(this.inputValue);
+        console.log(this.todos);
+        this.inputValue = '';
+    },
+    removeTodo: function (index) {
+        this.todos.splice(index, 1);
+    },
+    clearAll: function () {
+        this.todos = []
+    }
+    */
